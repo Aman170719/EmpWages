efeda17252cc50e17df64138dd067493ef579426
Aman <aman.kumar99499@gmail.com>
2022-06-16 11:04:40 +0530

    Master

---
+++ EmployeeWages/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Welcome to the Employee Wages Calculator");
+Console.WriteLine( "Attendance of the Emplyee based on thier Pesence");
+
+Random random = new Random();
+
+int num =random.Next(0,2);
+
+if (num == 0)
+{
+    Console.WriteLine("Emp Is Present");
+
+}
+else
+{
+    Console.WriteLine("Emp Is Absent");
+}
