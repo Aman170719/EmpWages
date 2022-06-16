f3f51449b1c254877401dfc3e00a43c307fd7c91
Aman <aman.kumar99499@gmail.com>
2022-06-16 11:32:24 +0530

    PartTimeSal

--- Empwages/Program.cs
+++ Empwages/Program.cs
@@ -3,6 +3,8 @@ Console.WriteLine("Attendance of the Emplyee based on thier Pesence");
 //constant values
 const int Emp_daily_wages = 20;
 const int Emp_work_hrs= 8;
+const int Emp_work_hrs_P_T = 4;
+
 
 // Random genertor for Attendacne
 Random random = new Random();
@@ -12,6 +14,7 @@ if (num == 0)
     Console.WriteLine("Emp Is Present");
     Console.WriteLine("Employe Daily salary is" + Emp_daily_wages * Emp_work_hrs);
     Console.WriteLine("Employe Monthly Salary is" + (Emp_daily_wages * Emp_work_hrs) * 20);
+    Console.WriteLine("Employee Woking Partime Salary is :" + (Emp_work_hrs_P_T * Emp_daily_wages) * 20);
 
 }
 else
