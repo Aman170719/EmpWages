cb8dd294c2bc3794e46b0198389c71926c623166
Aman <aman.kumar99499@gmail.com>
2022-06-16 11:26:14 +0530

    Emp Monthly Salary

---
+++ Empwages/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Welcome to the Employee Wages Calculator");
+Console.WriteLine("Attendance of the Emplyee based on thier Pesence");
+//constant values
+const int Emp_daily_wages = 20;
+const int Emp_work_hrs= 8;
+
+// Random genertor for Attendacne
+Random random = new Random();
+int num = random.Next(0, 2);
+if (num == 0)
+{
+    Console.WriteLine("Emp Is Present");
+    Console.WriteLine("Employe Daily salary is" + Emp_daily_wages * Emp_work_hrs);
+    Console.WriteLine("Employe Monthly Salary is" + (Emp_daily_wages * Emp_work_hrs) * 20);
+
+}
+else
+{
+    Console.WriteLine("Emp Is Absent");
+}
