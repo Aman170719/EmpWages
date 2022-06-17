ab40b180104f7d59b0273ae63eea2654a9db097e
Aman <aman.kumar99499@gmail.com>
2022-06-17 13:08:52 +0530

    initial Commit

--- Empwages/Program.cs
+++ Empwages/Program.cs
@@ -1,23 +1,32 @@
 ﻿Console.WriteLine("Welcome to the Employee Wages Calculator");
 Console.WriteLine("Attendance of the Emplyee based on thier Pesence");
 //constant values
-const int Emp_daily_wages = 20;
-const int Emp_work_hrs= 8;
-const int Emp_work_hrs_P_T = 4;
+int Emp_daily_wages = 20;
+int Emp_work_hrs= 0;
 
+//Random genertor for Attendacne
 
-// Random genertor for Attendacne
 Random random = new Random();
-int num = random.Next(0, 2);
-if (num == 0)
-{
-    Console.WriteLine("Emp Is Present");
-    Console.WriteLine("Employe Daily salary is" + Emp_daily_wages * Emp_work_hrs);
-    Console.WriteLine("Employe Monthly Salary is" + (Emp_daily_wages * Emp_work_hrs) * 20);
-    Console.WriteLine("Employee Woking Partime Salary is :" + (Emp_work_hrs_P_T * Emp_daily_wages) * 20);
+int num = random.Next(0, 3);
+//if (num == 0)
+//  Console.WriteLine("Emp Is Present");
 
-}
-else
-{
-    Console.WriteLine("Emp Is Absent");
-}
+ switch(num)
+    {
+        case 0:
+            Console.WriteLine("Empolye Present");
+            Emp_work_hrs = 8;
+            break;
+        case 1:
+            Console.WriteLine("Empolyee Present Part Time");
+            Emp_work_hrs = 4;
+            break;
+       case 2:
+            Console.WriteLine("Absent");
+            Emp_work_hrs = 0;
+            break;
+ }
+int Emp_wage = (Emp_work_hrs * Emp_daily_wages);
+Console.WriteLine( "Employee Wages is " + Emp_wage );
+Console.WriteLine("Emp Monthly Sal " + (Emp_wage * 20));
+
